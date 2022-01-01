c9055f747b4abe8d5e8b56e3f5d905cf7f10f83b
webdmamun <73527253+webdmamun@users.noreply.github.com>
2022-01-01 23:41:55 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Login from './Pages/Authentication/Login/Login';
-import Home from './Pages/Home/Home';
-import Header from './Pages/Shared/Header/Header';
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Agent from "./Pages/Agent/Agent";
+import Login from "./Pages/Authentication/Login/Login";
+import Registration from "./Pages/Authentication/Registration/Registration";
+import Blogs from "./Pages/Blogs/Blogs";
+import Contact from "./Pages/Contact/Contact";
+import Home from "./Pages/Home/Home";
+import Notfound from "./Pages/Notfound/Notfound";
+import Properties from "./Pages/Properties/Properties";
 
 function App() {
   return (
-    <div className="App">      
-      <BrowserRouter>
-                <Header/>
-              <Routes>
-                <Route path="/" element={<Home/>}></Route>
-                <Route path="/login" element={<Login/>}></Route>
-                
-                
-                
-              </Routes>
-                
-            </BrowserRouter>
-
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="blogs" element={<Blogs />}></Route>
+        <Route path="properties" element={<Properties />}></Route>
+        <Route path="agent" element={<Agent />}></Route>
+        <Route path="contact" element={<Contact />}></Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="registration" element={<Registration />}></Route>
+        <Route path="*" element={<Notfound />}></Route>
+      </Routes>
     </div>
   );
 }
