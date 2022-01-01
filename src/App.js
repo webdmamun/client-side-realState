19beb6cd1eab05fe1a50289b80b5347a6d8843b1
webdmamun <73527253+webdmamun@users.noreply.github.com>
2022-01-02 00:07:11 +0600

    header-update

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ import Contact from "./Pages/Contact/Contact";
 import Home from "./Pages/Home/Home";
 import Notfound from "./Pages/Notfound/Notfound";
 import Properties from "./Pages/Properties/Properties";
+import Header from "./Pages/Shared/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="blogs" element={<Blogs />}></Route>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,19 +1,32 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Urban Estate</Navbar.Brand>
+          <Navbar.Brand to="/">Urban Estate</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#features">Properties</Nav.Link>
+              <Link className="nav-link" to="properties">
+                Properties
+              </Link>
+              <Link className="nav-link " to="agent">
+                Agent
+              </Link>
+              <Link className="nav-link " to="blogs">
+                Blogs
+              </Link>
+              <Link className="nav-link" to="contact">
+                Contact
+              </Link>
+              {/* <Nav.Link href="#features">Properties</Nav.Link>
               <Nav.Link href="#pricing">Agent</Nav.Link>
               <Nav.Link href="#pricing">Blogs</Nav.Link>
-              <Nav.Link href="#pricing">Contact</Nav.Link>
+              <Nav.Link href="#pricing">Contact</Nav.Link> */}
             </Nav>
             <Nav>
               <Nav.Link href="#deets">More deets</Nav.Link>
