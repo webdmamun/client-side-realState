2b446a9a3316dc7e552635f4b861884eda6e97da
Mahdi Hasan Dip <mahdi.hasan1611@gmail.com>
2022-01-01 18:59:04 +0600

    Authentication starts

---
+++ src/Pages/Authentication/Registration/Registration.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Registration.css"
+
+const Registration = () => {
+    return (
+        <div>
+            <h1>Registration Page</h1>
+        </div>
+    );
+};
+
+export default Registration;
---
+++ src/Pages/Firebase/Firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCGm9u7IbW1WRl64NUAPmfvT43ExSaZbGU",
+  authDomain: "urban-estate-69bca.firebaseapp.com",
+  projectId: "urban-estate-69bca",
+  storageBucket: "urban-estate-69bca.appspot.com",
+  messagingSenderId: "287774668074",
+  appId: "1:287774668074:web:6d604a4e92da59003f9a53"
+};
+export default firebaseConfig;
