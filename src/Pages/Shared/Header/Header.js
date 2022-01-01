1f98975c668ff138e098709642a6f0280fcd9887
webdmamun <73527253+webdmamun@users.noreply.github.com>
2022-01-02 00:13:31 +0600

    header-bug-fix

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
   return (
     <>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand to="/">Urban Estate</Navbar.Brand>
+          <Navbar.Brand>
+            <Link className="nav-link" to="/">
+              Urban Estate
+            </Link>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
