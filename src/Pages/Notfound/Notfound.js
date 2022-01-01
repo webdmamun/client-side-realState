5116c28c5e9f765f78cac97b9b73c804d2a0633d
webdmamun <73527253+webdmamun@users.noreply.github.com>
2022-01-01 23:34:40 +0600

    Route-Setup

---
+++ src/Pages/Notfound/Notfound.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Notfound = () => {
+  return (
+    <Container>
+      <h2>this is Not Found page</h2>
+    </Container>
+  );
+};
+
+export default Notfound;
