45c0ad3256eda4b4d94807e997f6d0c15c003696
Mahdi Hasan Dip <mahdi.hasan1611@gmail.com>
2022-01-01 23:14:19 +0600

    registration page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './Pages/Authentication/Login/Login';
 import Home from './Pages/Home/Home';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <Home></Home>
+    <div className="App">      
+      <BrowserRouter>
+                <Header/>
+              <Routes>
+                <Route path="/" element={<Home/>}></Route>
+                <Route path="/login" element={<Login/>}></Route>
+                
+                
+                
+              </Routes>
+                
+            </BrowserRouter>
+
     </div>
   );
 }
