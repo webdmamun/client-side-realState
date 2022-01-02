592fadde45177950483b76b9ba94b139a380a91b
Mahdi Hasan Dip <mahdi.hasan1611@gmail.com>
2022-01-03 00:20:39 +0600

    carusel fixed

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -39,7 +39,7 @@ const Banner = () => {
           className="carousel-item active"
           data-bs-interval="10000"
         >
-          <img src={banner3} className="img-fluid" style={{ opacity: 0.5}} alt="..." />
+          <img src="https://i.ibb.co/mqPrGZ3/4.png" className="img-fluid" style={{ opacity: 0.5}} alt="..." />
           <div className="carousel-caption  text-center ">
             <h2 className="banner-text">WELCOME TO Urban Estate </h2>
             <p className="para">
@@ -55,7 +55,7 @@ const Banner = () => {
           </div>
         </div>
         <div className="carousel-item" data-bs-interval="2000">
-          <img style={{ opacity: 0.5}} src={banner2} className="img-fluid" alt="..." />
+          <img style={{ opacity: 0.5}} src="https://i.ibb.co/ygKBdY0/1.png" className="img-fluid" alt="..." />
           <div className="carousel-caption d-none d-md-block">
             <h2 className="banner-text">FIND YOUR DREAM Land</h2>
             <p className="para">
@@ -73,7 +73,7 @@ const Banner = () => {
           </div>
         </div>
         <div className="carousel-item">
-          <img style={{ opacity: 0.5}} src={banner1} className="img-fluid" alt="..." />
+          <img style={{ opacity: 0.5}} src="https://i.ibb.co/bHt5dFj/3.png" className="img-fluid" alt="..." />
           <div className="carousel-caption d-none d-md-block">
             <h2 className="banner-text">BEST PLACE FOR Buy Property!</h2>
             <p className="para">We are the best seller in the world</p>
