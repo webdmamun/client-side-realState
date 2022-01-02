5b279d9b7143c4dc1ded1c44d619f1882bdb6f71
Mahdi Hasan Dip <mahdi.hasan1611@gmail.com>
2022-01-02 17:14:35 +0600

    authentication

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -30,9 +30,11 @@ const Header = () => {
               </Nav.Link>
             </Nav>
             <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                Dank memes
+             
+              <Nav.Link className="text-light" href="">Welcome <span className="text-info">example@ gmail.com</span> </Nav.Link>
+              <Nav.Link as={Link} to="/login"><button className="btn btn-success"  >Log In</button></Nav.Link>
+              <Nav.Link eventKey={2} >
+                <button className="btn btn-danger" >Log out</button>
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
